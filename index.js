811956dff572b4007b0899b8f868817e7136f833
EvilMain1 <evilmain10@gmail.com>
2024-06-05 18:45:21 +0200

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["👑 | OfficialBot ChocolateMC","👨‍💻| Developed By EvilMain","🍫| chocolatemc.xyz"];
 
 
 let currentIndex = 0;
